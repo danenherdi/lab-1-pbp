6415b75c94ba24b66d6a919f485d3a638fa14e4e
DaHerdi <danendra.herdian1243@gmail.com>
2022-09-14 21:49:21 +0700

    Menginisiasi data berbentuk xml dan JSON

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from wishlist.views import show_wishlist
+from wishlist.views import show_wishlist_xml
+from wishlist.views import show_wishlist_json
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('xml/', show_wishlist_xml, name='show_wishlt_xml'),
+    path('json/<int:id>', show_wishlist_json, name='show_wishlist_json'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,6 +1,8 @@
 from multiprocessing import context
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_wishlist(request):
@@ -11,3 +13,13 @@ def show_wishlist(request):
     }
 
     return render(request, "wishlist.html", context)
+
+def show_wishlist_xml(request):
+    data_barang = BarangWishlist.objects.all()
+
+    return HttpResponse(serializers.serialize("xml", data_barang), content_type="application/xml")
+
+def show_wishlist_json(request, id):
+    data_barang = BarangWishlist.objects.filter(pk=id)
+    
+    return HttpResponse(serializers.serialize("json", data_barang), content_type="application/json")
